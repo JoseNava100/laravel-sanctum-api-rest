07cd9b49558502c55bc6424d2c7caf5308314e40
josenava <josenavaemanuel@outlook.es>
2025-01-13 21:10:29 -0700

    :)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Admin',
             'email' => 'admin@test.com',
+            'password' => bcrypt('12345678'),
         ]);
     }
 }

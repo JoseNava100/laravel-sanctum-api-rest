6180e537baca9f10fe9ebd72ee5ccd22c6dfdb06
josenava <josenavaemanuel@outlook.es>
2025-01-13 21:15:24 -0700

    ::)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Car;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,12 +14,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
 
         User::factory()->create([
             'name' => 'Admin',
             'email' => 'admin@test.com',
             'password' => bcrypt('12345678'),
+        ]);
+
+        Car::factory()->create([
+            'brand' => 'Lamborghini',
+            'model' => 'Aventador',
+            'year' => '2025',
+            'color' => 'Rojo',
+            'plate_number' => 'DDD4444',
         ]);
     }
 }

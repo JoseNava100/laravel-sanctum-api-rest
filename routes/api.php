45cf4fcb827c2d9068b5909ac15a722a7aa47ca5
josenava <josenavaemanuel@outlook.es>
2025-01-23 20:26:34 -0700

    Validation from conection in other frameworks xd

--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,14 @@
 
 use App\Http\Controllers\API\AuthenticationController;
 use App\Http\Controllers\API\CarController;
+use App\Http\Controllers\API\ValidationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('register', [AuthenticationController::class, 'register']);
 Route::post('login', [AuthenticationController::class, 'login']);
+
+Route::resource('validation', ValidationController::class);
 
 Route::middleware('auth:sanctum')->group(function(){
     Route::resource('cars', CarController::class);
